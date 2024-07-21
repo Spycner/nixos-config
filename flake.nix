11ced3ff8b4d994018a77fb68c3829c20b8cecd9
Spycner <pascal98kraus@gmail.com>
2024-07-21 19:23:04 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "My NixOS system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    devenv.url = "github:cachix/devenv";
+
+    agenix = {
+      url = "github:ryan4yin/ragenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    catppuccinifier = {
+      url = "github:lighttigerXIV/catppuccinifier";
+      inputs.nixpkgs.follows = "nipkgs";
+    };
+  };
+
+  outputs = { 
+    self, 
+    nixpkgs, 
+    home-manager,
+    systems,
+    ... 
+  } @ inputs: let
+    inherit (self) outputs;
+    lib = nixpkgs.lib // home-manager.lib;
+    forEachSystem = f: lib.genAttrs (import systems) (system: f pkgsFor.${system});
+    pkgsFor = lib.genAttrs (import systems) (
+      system:
+        import nixpkgs {
+          inherit system;
+	  config.allowUnfree = true;
+	}
+    );
+  in{
+    inherit lib;
+
+    # devShells = forEachSystem (pkgs: import ./shell.nix {inherit pkgs;});
+    formatter = ForEachSystem (pkgs: pkgs.alejandra);
+
+    nixosConfigurations = {
+      persephone = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [./hosts/persephone];
+	specialArgs = {
+          inherit inputs outputs;
+	};
+      };
+    };
+  };
+}
