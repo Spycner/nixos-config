e2ea9ed7aed9de9c8b0a95b3d6add668501b86e5
Pascal Kraus <pascal98kraus@gmail.com>
2024-07-24 10:44:54 +0200

    downloader with hash

--- packages/cursor.nix
+++ packages/cursor.nix
@@ -4,12 +4,13 @@
   version = "latest"; # Using "latest" as a placeholder
 
   src = pkgs.fetchurl {
+    # this will break if the version is updated.
+    # unfortunately, i couldn't seem to find a url that
+    # points to a specific version.
+    # alternatively, download the appimage manually and
+    # include it via src = ./cursor.AppImage, instead of fetchurl
     url = "https://downloader.cursor.sh/linux/appImage/x64";
-    downloadToTemp = true;
-    postFetch = ''
-      mv $downloadedFile $out
-      chmod +x $out
-    '';
+    hash = "sha256-Fsy9OVP4vryLHNtcPJf0vQvCuu4NEPDTN2rgXO3Znwo=";
   };
   appimageContents = pkgs.appimageTools.extract {inherit pname version src; };
 in
