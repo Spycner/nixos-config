fad6afc6e612e7c4e609c495c4489b65c40be517
Spycner <pascal98kraus@gmail.com>
2024-07-25 21:01:00 +0200

    building state

--- hosts/persephone/displaylink.nix
+++ hosts/persephone/displaylink.nix
@@ -1,17 +1,6 @@
-{ pkgs, config, lib, ... }:
-
-let
-  displayLinkPkg = pkgs.displaylink.overrideAttrs (oldAttrs: {
-    version = "6.0.0-24";
-    src = pkgs.fetchzip {
-      url = "https://www.synaptics.com/sites/default/files/exe_files/2024-05/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu6.0-EXE.zip";
-      sha256 = lib.fakeSha256; # Replace this with the actual hash after the first attempt
-      name = "displaylink-600.zip";
-    };
-  });
-in
+{ pkgs, config, ... }:
 {
-  environment.systemPackages = [ displayLinkPkg ];
+  environment.systemPackages = with pkgs; [ displaylink ];
 
   services.xserver.videoDrivers = [ "displaylink" "modesetting" ];
 
